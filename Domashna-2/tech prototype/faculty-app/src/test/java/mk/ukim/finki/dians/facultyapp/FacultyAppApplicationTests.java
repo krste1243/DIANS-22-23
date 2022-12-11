356870b1fc6bed43d55ed18837e6461021c002e3
gorgilazarev3 <gorgi.lazarev3@gmail.com>
2022-12-11 17:19:33 +0100

    Updated all files for homework 2

---
+++ Domashna-2/tech prototype/faculty-app/src/test/java/mk/ukim/finki/dians/facultyapp/FacultyAppApplicationTests.java
@@ -0,0 +1,15 @@
+package mk.ukim.finki.dians.facultyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootTest
+@ServletComponentScan
+class FacultyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
